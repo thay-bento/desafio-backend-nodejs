c5f41c93c5333ef45f155435057020b353d36743
thay-bento <thayna.silva@stone.com.br>
2022-07-28 14:49:48 -0300

    feature: whitelist and fobidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { CustomersModule } from './customers/customers.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CustomersModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
