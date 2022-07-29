4bdb62a6d1e80f1b4ec7e5474ff8095edd4e4931
thay-bento <thayna.silva@stone.com.br>
2022-07-29 17:14:07 -0300

    feature: authetication

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -9,21 +9,27 @@ import {
   ParseUUIDPipe,
   Post,
   Put,
+  SetMetadata,
+  UseGuards,
 } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { Customers } from './customers';
 import { CustomersDto } from '../dto/customers.dto';
+import { RolesGuard } from '../auth/roles.guard.auth';
 
 @Controller('customers')
+@UseGuards(RolesGuard)
 export class CustomersController {
   constructor(private readonly customerService: CustomersService) {}
 
   @Post()
+  @SetMetadata('roles', ['admin', 'user'])
   async create(@Body() customer: CustomersDto): Promise<Customers> {
     return this.customerService.create(customer);
   }
 
   @Put(':id')
+  @SetMetadata('roles', ['admin', 'user'])
   async updateCustomer(
     //validando se é um UUID com ParseUUIDPipe()
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -35,6 +41,7 @@ export class CustomersController {
   }
 
   @Get(':id')
+  @SetMetadata('roles', ['admin', 'user'])
   getById(
     @Param('id', new ParseUUIDPipe()) customer: Customers,
   ): Promise<string> {
@@ -42,6 +49,7 @@ export class CustomersController {
   }
 
   @Delete(':id')
+  @SetMetadata('roles', ['admin', 'user'])
   @HttpCode(HttpStatus.NO_CONTENT)
   async destroy(@Param('id', new ParseUUIDPipe()) customer: Customers) {
     await this.customerService.destroy(customer);
